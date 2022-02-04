3ee2e76d15573958dd74107683125eb025259fae
Yuki Matsumoto <abc123931@gmail.com>
2022-02-04 09:44:17 +0900

    ✨ Add keyboard.

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -2,6 +2,7 @@ import { AntDesign } from "@expo/vector-icons";
 import { Avatar, Box, Button, Flex, HStack, Icon, Input, ScrollView, Text, useTheme, VStack } from "native-base";
 import type { ColorType } from "native-base/lib/typescript/components/types";
 import type { ReactNode, VFC } from "react";
+import { InputAccessoryView } from "react-native";
 
 type AppLayoutProps = {
   children: ReactNode;
@@ -31,23 +32,26 @@ export const AppLayout: VFC<AppLayoutProps> = (props) => {
       <ScrollView w="100%" flexGrow={1}>
         {props.children}
       </ScrollView>
-      <Box w="100%" bg="#6200ee">
-        <VStack px={6} py={3} space={2.5}>
-          <Input
-            variant="rounded"
-            placeholder="タスクを入力する"
-            height={36}
-            py={2.5}
-            px={4}
-            color={theme.colors.trueGray[100]}
-          />
-          <HStack alignItems="center" justifyContent="space-between">
-            <StatusButton text="今日する" color={theme.colors.rose[500]} />
-            <StatusButton text="明日する" color={theme.colors.orange[400]} />
-            <StatusButton text="今度する" color={theme.colors.amber[400]} />
-          </HStack>
-        </VStack>
-      </Box>
+      <InputAccessoryView nativeID="inputAccessoryViewID">
+        <Box w="100%" bg="#6200ee">
+          <VStack px={6} py={3} space={2.5}>
+            <Input
+              variant="rounded"
+              placeholder="タスクを入力する"
+              height={36}
+              py={2.5}
+              px={4}
+              color={theme.colors.trueGray[100]}
+              inputAccessoryViewID="inputAccessoryViewID"
+            />
+            <HStack alignItems="center" justifyContent="space-between">
+              <StatusButton text="今日する" color={theme.colors.rose[500]} />
+              <StatusButton text="明日する" color={theme.colors.orange[400]} />
+              <StatusButton text="今度する" color={theme.colors.amber[400]} />
+            </HStack>
+          </VStack>
+        </Box>
+      </InputAccessoryView>
     </Flex>
   );
 };
