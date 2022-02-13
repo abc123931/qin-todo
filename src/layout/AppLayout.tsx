5ea375c9fe03d6e81472abe738abaa2fbefe9e1b
Yuki Matsumoto <abc123931@gmail.com>
2022-02-13 16:07:31 +0900

    ✨ Update profile image.

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,15 @@
 import { Avatar, Box, Flex, HStack, Text, useTheme, View, VStack } from "native-base";
 import type { ReactNode, VFC } from "react";
 import { FooterKeyboard } from "src/layout/FooterKeyboard";
+import { sessionState } from "src/valtio/session";
+import { useSnapshot } from "valtio";
 
 type AppLayoutProps = {
   children: ReactNode;
 };
 export const AppLayout: VFC<AppLayoutProps> = (props) => {
   const theme = useTheme();
+  const sessionSnap = useSnapshot(sessionState);
 
   return (
     <Flex justifyContent="space-between" h="100%" bgColor={theme.colors.white}>
@@ -22,9 +25,11 @@ export const AppLayout: VFC<AppLayoutProps> = (props) => {
             h={36}
             my={2.5}
             source={{
-              uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+              uri: sessionSnap.session?.user?.user_metadata?.avatar_url + "test",
             }}
-          />
+          >
+            <Avatar w={36} h={36} my={2.5} source={require("/assets/dummy_profile.png")} />
+          </Avatar>
         </HStack>
       </VStack>
       {/* TODO: 修正必要 なぜかheightを指定しないといけない */}
