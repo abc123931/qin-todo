1dbe01ad04aafe0452c039d5e8d9d412b89648ce
Yuki Matsumoto <abc123931@gmail.com>
2022-02-20 11:53:41 +0900

    💄 Update setting page design.

--- src/screen/SettingScreen/SettingScreen.tsx
+++ src/screen/SettingScreen/SettingScreen.tsx
@@ -1,11 +1,27 @@
-import { Text } from "native-base";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Box, HStack, Icon, Pressable, Text, VStack } from "native-base";
 import type { VFC } from "react";
 import { AllLayout } from "src/layout/AllLayout";
+import type { RootStackParamList } from "src/MainNavigator";
 
 export const SettingScreen: VFC = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList, "setting">>();
+
   return (
     <AllLayout>
-      <Text>設定画面</Text>
+      <VStack w="100%" h="100%">
+        <HStack justifyContent="space-between" alignItems="center" px={6} py={3}>
+          <Pressable onPress={() => navigation.goBack()}>
+            <Icon size={22} as={Ionicons} name={"close-outline"} />
+          </Pressable>
+          <Text fontSize="lg" fontWeight="bold">
+            設定
+          </Text>
+          <Box w={22} h={22} />
+        </HStack>
+      </VStack>
     </AllLayout>
   );
 };
