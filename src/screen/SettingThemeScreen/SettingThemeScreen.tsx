cedc3365c6c59a491f9ce2bedf832e170acd6259
Yuki Matsumoto <abc123931@gmail.com>
2022-02-22 20:54:49 +0900

    ✨ Update theme screen.

--- src/screen/SettingThemeScreen/SettingThemeScreen.tsx
+++ src/screen/SettingThemeScreen/SettingThemeScreen.tsx
@@ -1,32 +1,43 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Heading, HStack, Icon, Pressable, useTheme } from "native-base";
+import { Box, Heading, HStack, Icon, Pressable, useTheme } from "native-base";
 import type { VFC } from "react";
 import { SettingLayout } from "src/layout/SettingLayout";
-import { storeTheme } from "src/valtio/theme";
+import { storeTheme, themeState } from "src/valtio/theme";
+import { useSnapshot } from "valtio";
+
+const themeTypes: ("os" | "light" | "dark")[] = ["os", "light", "dark"];
 
 export const SettingThemeScreen: VFC = () => {
-  const theme = useTheme();
-
   return (
     <SettingLayout title="テーマ" backIconName="chevron-back-outline">
-      <Pressable onPress={() => storeTheme("os")}>
-        <HStack py={3.5} justifyContent="space-between">
-          <Heading size="sm">OSの設定に合わせる</Heading>
-          <Icon size={22} as={Ionicons} name="checkmark" color={theme.colors.primary[400]} />
-        </HStack>
-      </Pressable>
-      <Pressable onPress={() => storeTheme("light")}>
-        <HStack py={3.5} justifyContent="space-between">
-          <Heading size="sm">ライト</Heading>
-          <Icon size={22} as={Ionicons} name="checkmark" color={theme.colors.primary[400]} />
-        </HStack>
-      </Pressable>
-      <Pressable onPress={() => storeTheme("dark")}>
-        <HStack py={3.5} justifyContent="space-between">
-          <Heading size="sm">ダーク</Heading>
-          <Icon size={22} as={Ionicons} name="checkmark" color={theme.colors.primary[400]} />
-        </HStack>
-      </Pressable>
+      {themeTypes.map((type) => {
+        return (
+          <Box key={type}>
+            <Row type={type} />
+          </Box>
+        );
+      })}
     </SettingLayout>
+  );
+};
+
+type RowProps = {
+  type: "os" | "light" | "dark";
+};
+const Row: VFC<RowProps> = (props) => {
+  const theme = useTheme();
+  const snap = useSnapshot(themeState);
+
+  return (
+    <Pressable onPress={() => storeTheme(props.type)}>
+      <HStack py={3.5} justifyContent="space-between">
+        <Heading size="sm">
+          {props.type === "os" ? "OSの設定に合わせる" : props.type === "light" ? "ライト" : "ダーク"}
+        </Heading>
+        {snap.theme === props.type ? (
+          <Icon size={22} as={Ionicons} name="checkmark" color={theme.colors.primary[400]} />
+        ) : null}
+      </HStack>
+    </Pressable>
   );
 };
